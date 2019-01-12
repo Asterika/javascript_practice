13a7e5554b69fa9b19d0f2c746a69c97d85ced14
Krystal Williams <asterika@Krystals-MBP.lan>
2019-01-11 18:41:35 -0800

    Complete boolean logic, added files

---
+++ js/s2.15_booleanlogic.js
@@ -0,0 +1,21 @@
+/************
+* Boolean Logic
+*/
+
+// Logical Operators:
+// AND (&&) //ALL are true
+// OR (||) //ONE is true
+// NOT (!) //inverts true and false
+
+var firstName = 'John';
+var age = 16;
+
+if (age < 13) {
+  console.log(firstName + ' is a boy.');
+} else if (age >= 13 && age < 20) { //Between 13 and 20 = age >= 13 AND age < 20
+  console.log(firstName + ' is a teenager.');
+} else if (age >= 20 && age < 30) {
+  console.log(firstName + ' is a young man.');
+} else {
+  console.log(firstName + ' is a man.');
+}
